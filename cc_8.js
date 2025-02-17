8dd3952928046426cc72c49e5b1391dbe65b502c
Natalie Mauricio-Cruz <mauriciocruzn@usf.edu>
2025-02-16 19:23:06 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -9,3 +9,11 @@ calculateSalary(5000, 500, 0.1); //Output: Net Salary: $ 5000.00
 calculateSalary(7000, 1000, 0.15); //Output: Net Salary: $6950.00
 
 //Task 2 Function Expression
+function calculateDiscount(price, discountRate) {
+    let finalPrice = price - (price * discountRate)
+    console.log (`Net Salary: $${finalPrice.toFixed(2)}`)
+    return finalPrice
+};
+
+calculateDiscount(100, 0.2); // Output: Final Price: $80.00
+calculateDiscount(250, 0.15); // Output: Final Price: $212.50
